a1bb082f8e0914374b70d1f542719b4b8bfd1b33
Jalal Yadegar <jalaldgr@gmail.com>
2025-12-04 10:43:32 +0330

    fix(build):set standalone output in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-  },
+  output: 'standalone',
   images: {
     domains: ['localhost', 'example.com'], // دامنه‌هایی که تصویر از آن‌ها لود می‌شود
   },

5e36994c46edef7bb72d451c2f4b434813d1e518
Jalal Yadegar <jalaldgr@gmail.com>
2025-12-04 10:30:59 +0330

    fix(frontend): config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true, // فعال‌سازی App Router (اختیاری اما پیشنهادی)
   },
   images: {
     domains: ['localhost', 'example.com'], // دامنه‌هایی که تصویر از آن‌ها لود می‌شود
   },
-  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
